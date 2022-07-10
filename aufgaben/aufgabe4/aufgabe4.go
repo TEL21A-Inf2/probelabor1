5696cdf419b653dc29176275654c4d28fea337ff
Reiner Hüchting <dev@reinerhuechting.de>
2022-07-10 13:47:24 +0200

    Aufgabe 4 hinzugefügt.

---
+++ aufgaben/aufgabe4/aufgabe4.go
@@ -0,0 +1,20 @@
+// Thema der Aufgabe: Bäume
+// Erreichbare Punkte: 6
+// Bewertung:
+
+package aufgabe4
+
+/* Aufgabenstellung:
+ * In der Datei bintrree.go ist ein Datentyp für einen binären Suchbaum definiert.
+ *
+ * Implementieren Sie für diesen Datentyp die unten vorgegebene Methode HasOddElementNumber().
+ * Die Methode soll true liefern, falls der Baum eine ungerade Anzahl an Elementen hat.
+ */
+
+// Verringert den kleinsten Wert im Baum um 1.
+func (tree *BinTree) HasOddElementNumber() bool {
+	if tree.Empty() {
+		return false
+	}
+	return (tree.Left.HasOddElementNumber() == tree.Right.HasOddElementNumber())
+}
